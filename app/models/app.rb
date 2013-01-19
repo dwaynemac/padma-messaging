37303ffddd22005e7cd7662ed4042af3db0fa6f1
Dwayne Macgowan <dwaynemac@gmail.com>
2013-01-19 02:48:16 -0300

    bugfix

--- app/models/app.rb
+++ app/models/app.rb
@@ -5,7 +5,7 @@ class App < ActiveRecord::Base
   validates_uniqueness_of :name
 
   has_many :app_allowed_keys, dependent: :destroy
-  has_many :allowed_message_keys, through: :app_allowed_keys, class_name: 'MessageKey'
+  has_many :allowed_message_keys, through: :app_allowed_keys, class_name: 'MessageKey', source: :message_key
 
   has_many :messages
 
