8d2249a3a879fc2f616657543bbc220e06ce3dc0
Dwayne Macgowan <dwaynemac@gmail.com>
2013-01-19 02:52:49 -0300

    set key by name

--- app/models/app_allowed_key.rb
+++ app/models/app_allowed_key.rb
@@ -1,6 +1,9 @@
+require 'set_by_name'
 # many-to-many relationship between App and MessageKey
 class AppAllowedKey < ActiveRecord::Base
-  attr_accessible :app_id, :message_key_id
+  include SetByName
+
+  attr_accessible :app_name, :key_name
 
   belongs_to :app
   belongs_to :message_key
